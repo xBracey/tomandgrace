4aa1931ff6ef100e78bbe1c47a77272aac7a024c
Thomas Brace <tommy-brace-22@hotmail.com>
2020-01-02 11:05:06 +0000

    Add template for about page

---
+++ tomandgrace/web/app/themes/tomandgrace/resources/views/partials/about.blade.php
@@ -0,0 +1,13 @@
+<article class="about">
+    <div class="tom-content">
+        <div class="content-container">
+            {!! get_field('tom_content') !!}
+        </div>
+    </div>
+
+    <div class="grace-content">
+        <div class="content-container">
+            {!! get_field('grace_content') !!}
+        </div>
+    </div>
+</article>
--- tomandgrace/web/app/themes/tomandgrace/resources/views/partials/content-page.blade.php
+++ tomandgrace/web/app/themes/tomandgrace/resources/views/partials/content-page.blade.php
@@ -1,2 +1,5 @@
-@php the_content() @endphp
-{!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
+<article>
+    <div class="content">
+        {!! the_content() !!}
+    </div>
+</article>
