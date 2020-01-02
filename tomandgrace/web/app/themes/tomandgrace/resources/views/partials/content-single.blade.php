ddcdece31193a942b6ebe8085565e2fa97ba35b0
Thomas Brace <tommy-brace-22@hotmail.com>
2020-01-02 09:52:44 +0000

    Add single blog page

--- tomandgrace/web/app/themes/tomandgrace/resources/views/partials/content-single.blade.php
+++ tomandgrace/web/app/themes/tomandgrace/resources/views/partials/content-single.blade.php
@@ -1,13 +1,16 @@
-<article @php post_class() @endphp>
-  <header>
-    <h1 class="entry-title">{!! get_the_title() !!}</h1>
-    @include('partials/entry-meta')
-  </header>
-  <div class="entry-content">
-    @php the_content() @endphp
-  </div>
-  <footer>
-    {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
-  </footer>
-  @php comments_template('/partials/comments.blade.php') @endphp
+<article>
+    <div class="blog-header-container">
+        <div class="blog-header">
+            <div class="info">
+                <h1>{{ get_the_title() }}</h1>
+                <h2>By {{ get_field('tom_or_grace') == "Tom" ? "Tom Brace" : "Grace Marsey" }}</h2>
+            </div>
+        </div>
+        <img src="{{get_the_post_thumbnail_url(null, 'large')}}" alt="Post Header Image" />
+        <div class="light-background"></div>
+    </div>
+
+    <div class="content">
+        {{ the_content() }}
+    </div>
 </article>
